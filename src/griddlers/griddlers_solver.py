2d4e1b3b644b1ed5740fa46ddb6d66bf7a26fc8e
Sagi Shadur <saroad2@gmail.com>
2021-03-30 14:08:42 +0300

    Replace deque with set

--- src/griddlers/griddlers_solver.py
+++ src/griddlers/griddlers_solver.py
@@ -1,4 +1,3 @@
-from collections import deque
 from typing import List
 
 from griddlers.cell_mark import CellsLine
@@ -30,9 +29,9 @@ class GriddlersSolver:
                 differ_indices = self.calculate_differ_indices(row, result_row)
                 if len(differ_indices) != 0:
                     game.set_row(row_index=row_index, row=result_row)
-                    rows_queue.appendleft(row_index)
+                    rows_queue.add(row_index)
                     for i in differ_indices:
-                        columns_queue.appendleft(i)
+                        columns_queue.add(i)
             while len(columns_queue) != 0:
                 column_index = columns_queue.pop()
                 column, column_instructions = game.get_column_and_instructions(
@@ -42,23 +41,25 @@ class GriddlersSolver:
                 differ_indices = self.calculate_differ_indices(column, result_column)
                 if len(differ_indices) != 0:
                     game.set_column(column_index=column_index, column=result_column)
-                    columns_queue.appendleft(column_index)
+                    columns_queue.add(column_index)
                     for i in differ_indices:
-                        rows_queue.appendleft(i)
+                        rows_queue.add(i)
 
     def run_strategies(self, line: CellsLine, instructions: List[int]):
+        if line.is_completed:
+            return line
         for strategy in self.strategies:
             line = strategy.solve(line=line, instructions=instructions)
         return line
 
     @classmethod
     def get_rows_and_columns_queues(cls, game):
-        rows_queue = deque()
-        columns_queue = deque()
+        rows_queue = set()
+        columns_queue = set()
         for i in range(game.rows):
-            rows_queue.appendleft(i)
+            rows_queue.add(i)
         for i in range(game.columns):
-            columns_queue.appendleft(i)
+            columns_queue.add(i)
         return rows_queue, columns_queue
 
     @classmethod
