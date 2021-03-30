b4047000d406525d0be4aea7d493326c176debbd
Sagi Shadur <saroad2@gmail.com>
2021-03-30 15:31:36 +0300

    Add sections property to cells line

--- src/griddlers/cells_line.py
+++ src/griddlers/cells_line.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import List
 
 from griddlers.cell_mark import CellMark
+from griddlers.cells_section import CellsSection
 
 
 @dataclass(repr=False)
@@ -16,6 +17,31 @@ class CellsLine:
     @property
     def is_completed(self):
         return all(cell != CellMark.EMPTY for cell in self.cells)
+
+    @property
+    def sections(self):
+        sections = []
+        if len(self.cells) == 0:
+            return sections
+        start_index = 0
+        mark = self.cells[0]
+        for i in range(1, len(self.cells)):
+            if self.cells[i] != mark:
+                sections.append(CellsSection(start=start_index, end=i - 1, mark=mark))
+                start_index = i
+                mark = self.cells[i]
+        sections.append(
+            CellsSection(start=start_index, end=len(self.cells) - 1, mark=mark)
+        )
+        return sections
+
+    @property
+    def filled_sections(self):
+        return [section for section in self.sections if section.mark == CellMark.FILLED]
+
+    @property
+    def empty_sections(self):
+        return [section for section in self.sections if section.mark == CellMark.EMPTY]
 
     def inverse(self) -> "CellsLine":
         return CellsLine(cells=self.cells[::-1])
--- tests/test_cells_line.py
+++ tests/test_cells_line.py
@@ -1,5 +1,6 @@
 from griddlers.cells_line import CellsLine
 from griddlers.cell_mark import CellMark
+from griddlers.cells_section import CellsSection
 
 
 def test_parse_cells_line():
@@ -105,4 +106,39 @@ def test_cells_line_convert_to_list():
         CellMark.FILLED,
         CellMark.EMPTY,
         CellMark.CROSSED
+    ]
+
+
+def test_cells_line_sections():
+    assert CellsLine.parse_line("X__OOO_X").sections == [
+        CellsSection(start=0, end=0, mark=CellMark.CROSSED),
+        CellsSection(start=1, end=2, mark=CellMark.EMPTY),
+        CellsSection(start=3, end=5, mark=CellMark.FILLED),
+        CellsSection(start=6, end=6, mark=CellMark.EMPTY),
+        CellsSection(start=7, end=7, mark=CellMark.CROSSED),
+    ]
+
+
+def test_cells_line_sections_after_set():
+    cells_line = CellsLine.parse_line("X__OOO_X")
+    cells_line[6] = CellMark.FILLED
+    assert cells_line.sections == [
+        CellsSection(start=0, end=0, mark=CellMark.CROSSED),
+        CellsSection(start=1, end=2, mark=CellMark.EMPTY),
+        CellsSection(start=3, end=6, mark=CellMark.FILLED),
+        CellsSection(start=7, end=7, mark=CellMark.CROSSED),
+    ]
+
+
+def test_cells_line_filled_sections():
+    assert CellsLine.parse_line("X_OOXO_X").filled_sections == [
+        CellsSection(start=2, end=3, mark=CellMark.FILLED),
+        CellsSection(start=5, end=5, mark=CellMark.FILLED),
+    ]
+
+
+def test_cells_line_empty_sections():
+    assert CellsLine.parse_line("X__OXO_X").empty_sections == [
+        CellsSection(start=1, end=2, mark=CellMark.EMPTY),
+        CellsSection(start=6, end=6, mark=CellMark.EMPTY),
     ]
